d578f29011b0a9b01a58e6930128ea382be21f90
danprophet <76520987+danprophet@users.noreply.github.com>
2021-05-03 19:56:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# ------------>>>>>>>> RUN THIS CODE CELL <<<<<<<<------------
+# === CELL TYPE: IMPORTS AND SETUP 
+
+import time      # for testing use only
+import os         # for testing use only
+
+import bs4
+from bs4 import BeautifulSoup  
+import pandas as pd
+import scipy as sc
+import numpy as np
+import requests
+
+
+URL = "https://apis.cbs.gov.il/series/catalog/path"
+# PARAMS = {'format': 'json', 'download': 'false', 'id': '20,1,1,null,963', 'name': 'התאונות כל',
+#           'pathDesc': 'null'}
+
+PARAMS = {'format': 'json', 'download': 'false', 'id': '20'}
+
+# URL2='https://apis.cbs.gov.il/series/catalog/path?id=2,1,1,2,963&format=xml&download=false'
+
+r = requests.get(url=URL, params=PARAMS)
+print(r)
+
+site = r.text
+
+
+
+save = open("test123.txt", 'w', encoding="utf-8")
+save.write(site)
+save.close()
+
+
+
+#
+#
+# soup_obj = BeautifulSoup(site)
+
+
+# print(soup_obj)
+
